1141caa2f940e99d24f1e2b9d6c7e33bdb64fb74
abhisheksaran <abhishekbishnoi695@gmail.com>
2020-10-28 09:49:56 +0530

    Test Harness Class Intialisation

---
+++ src/infrastructure/validation/testing/TestHarness.java
@@ -0,0 +1,15 @@
+/**
+ * @author        Abhishek Saran
+ * @coauthor      Manas Sanjay
+ * @module        Infrastructure module 
+ * @team          Test Harness
+ * @description   Test Harness main class
+ * @summary       Methods such as runAll(), runByName, etc. provided to run test cases sets.
+ */
+
+package infrastructure.validation.testing;
+
+public class TestHarness{
+
+
+}
